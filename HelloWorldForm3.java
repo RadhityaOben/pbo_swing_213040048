07fab1bc4ab145a6ec51f58196edfa82d708994b
MineFrostID <obenunity56@gmail.com>
2023-06-10 15:26:29 +0700

    Pengumpulan Tugas

---
+++ HelloWorldForm3.java
@@ -0,0 +1,44 @@
+package Tugas;
+
+import javax.swing.*;
+
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+public class HelloWorldForm3 extends JFrame implements ActionListener{
+    private JButton jumlah;
+    private JTextField textField1;
+    private JTextField textField2;
+
+    private int nilai1 = 0;
+    private int nilai2 = 0;
+    private int hasil = 0;
+
+    public HelloWorldForm3() {
+        jumlah = new JButton("Jumlah");
+        jumlah.addActionListener(this);
+        textField1 = new JTextField();
+        textField2 = new JTextField();
+
+        setTitle("Penjumlahan");
+        setLayout(new GridLayout(3, 1));
+        add(textField1);
+        add(textField2);
+        add(jumlah);
+        setSize(200, 150);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(!textField1.getText().equals("") && !textField1.getText().equals("")){
+            nilai1 = Integer.parseInt(textField1.getText());
+            nilai2 = Integer.parseInt(textField2.getText());
+        }
+            hasil = nilai1 + nilai2;
+
+        JOptionPane.showMessageDialog(this, "Hasil penjumlahan adalah " + hasil);
+    }
+}
---
+++ ProgramUtama.java
@@ -0,0 +1,9 @@
+package Tugas;
+
+import javax.swing.JFrame;
+
+public class ProgramUtama {
+    public static void main(String[] args) {
+        JFrame form = new HelloWorldForm3();
+    }
+}
